95c3b3da6e86a0c7da2f6ef3a0b6efe5b14e1ffd
Brunodesouzadosanjos <brunodesouzadosanjos21@gmail.com>
2025-04-04 13:22:36 -0300

    adicionando elementos

--- interface.py
+++ interface.py
@@ -1,1 +1,7 @@
-from tkinter import
+import customtkinter as ctk
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('blue')
+app = ctk.CTk()
+app.geometry("500x500")
+app.title('Calculadora')
+app.mainloop()
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+import interface
 import function
-function.calcular()
+
+interface
